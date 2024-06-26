d4426147f65cf626718840a4049fa3a58013849e
PanFirdavs <163105277+PanFirdavs@users.noreply.github.com>
2024-06-26 19:46:45 +0500

    tailwind-trip

---
+++ src/components/Offers.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Offers = () => {
+  return (
+    <div className='max-w-[900px] m-auto px-4 py-12 flex flex-wrap justify-between content-center '>
+      <p className='text-lg font-bold p-4 mb-4 text-gray-700 border-b border-green-400'>Booking Incentives</p>
+      <p className='text-lg font-bold p-4  mb-4 text-gray-700 border-b border-green-400'>Competitive Rates</p>
+      <p className='text-lg font-bold p-4  mb-4 text-gray-700 border-b border-green-400'>Book With Flexibility</p>
+      <p className='text-lg font-bold p-4  mb-4 text-gray-700 border-b border-green-400'>Trip Insurance</p>
+    </div>
+  );
+};
+
+export default Offers;
